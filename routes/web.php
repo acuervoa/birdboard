69d918ed3b9af83ee0dba543b5b4db8ab6a03ea8
acuervoa <kuerbo@gmail.com>
2019-03-20 16:43:42 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get( '/', function () {
     return view( 'welcome' );
 } );
 
-Route::group(['middleware' => 'auth'], function() {
+Route::group( ['middleware' => 'auth'], function () {
 
     Route::get( '/projects', 'ProjectsController@index' );
-    Route::get('/projects/create', 'ProjectsController@create');
+    Route::get( '/projects/create', 'ProjectsController@create' );
     Route::get( '/projects/{project}', 'ProjectsController@show' );
     Route::get( '/projects/{project}/edit', 'ProjectsController@edit' );
     Route::patch( '/projects/{project}', 'ProjectsController@update' );
@@ -28,8 +28,8 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post( '/projects/{project}/tasks', 'ProjectTasksController@store' );
     Route::patch( '/projects/{project}/tasks/{task}', 'ProjectTasksController@update' );
 
-    Route::get('/home', 'HomeController@index')->name('home');
-});
+    Route::get( '/home', 'HomeController@index' )->name( 'home' );
+} );
 
 Auth::routes();
 
