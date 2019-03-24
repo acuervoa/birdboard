277a9174c9b11aefd8ea88cdba84c692596b1e92
acuervoa <kuerbo@gmail.com>
2019-03-24 20:47:43 +0100

    End episode 29

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -40,14 +40,18 @@ class ProjectsController extends Controller {
 
     public function update(Project $project)
     {
-
         $this->authorize( 'update', $project );
-
         $project->update( $this->validateRequest() );
-
         return redirect( $project->path() );
     }
 
+    public function destroy(Project $project)
+    {
+        $this->authorize( 'update', $project );
+        $project->delete();
+        return redirect('/projects');
+    }
+    
     /**
      * @return array
      */
--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -45,7 +45,7 @@ trait RecordsActivity {
             return static::$recordableEvents;
         }
 
-        return ['created', 'updated', 'deleted'];
+        return ['created', 'updated'];
 
     }
 
