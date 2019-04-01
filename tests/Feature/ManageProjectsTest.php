abd6ee0bc1513f2c53d33e80adb3aa0d26f97eb0
acuervoa <kuerbo@gmail.com>
2019-04-01 20:43:41 +0200

    End epidode 35

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -30,33 +30,22 @@ class ManageProjectsTest extends TestCase {
     /** @test */
     public function a_user_can_create_a_project()
     {
+
         $this->signIn();
 
         $this->get( '/projects/create' )->assertStatus( 200 );
 
-        $attributes = [
-            'title'       => $this->faker->sentence,
-            'description' => $this->faker->sentence,
-            'notes'       => 'General notes here.'
-        ];
-
-        $response = $this->post( '/projects', $attributes );
-
-        $project = Project::where( $attributes )->first();
-
-        $response->assertRedirect( $project->path() );
-
-
-        $this->get( $project->path() )
+        $this->followingRedirects()
+            ->post('/projects', $attributes = factory( Project::class )->raw() )
             ->assertSee( $attributes['title'] )
-            ->assertSee( $attributes['description'] )
+            ->assertSee( Str::limit( $attributes['description'], 100 ) )
             ->assertSee( $attributes['notes'] );
     }
 
     /** @test */
     public function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
     {
-        $project = tap(ProjectFactory::create())->invite( $this->signIn() );
+        $project = tap( ProjectFactory::create() )->invite( $this->signIn() );
 
         $this->get( '/projects' )
             ->assertSee( $project->title );
@@ -73,9 +62,13 @@ class ManageProjectsTest extends TestCase {
         $this->delete( $project->path() )
             ->assertRedirect( '/login' );
 
-        $this->signIn();
+        $user = $this->signIn();
 
         $this->delete( $project->path() )->assertStatus( 403 );
+
+        $project->invite( $user );
+
+        $this->actingAs( $user )->delete( $project->path() )->assertStatus( 403 );
     }
 
     /** @test */
